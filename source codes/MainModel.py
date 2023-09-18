490e09439a5bca9b22310d6ae8038619ea1dfc5e
mohamadch91 <mohamadchoupan80@gmail.com>
2023-09-19 03:06:58 +0330

    feat: complete main Model class

--- source codes/MainModel.py
+++ source codes/MainModel.py
@@ -38,6 +38,7 @@ class MainModel(torch.nn):
         super().__init__()
         self.dataset_path=dataset_path
         self.model=model
+        self.optimizer=None
         self.data_loader=data_loader
         self.data_set={
             "train":None,
@@ -53,6 +54,7 @@ class MainModel(torch.nn):
         self.test_f1_score=[]
         self.train_precision=[]
         self.test_precision=[]
+    
         
         
     def read_dataset(self,reader,valid,force_to_cal=False):
@@ -72,16 +74,69 @@ class MainModel(torch.nn):
         else:
             self.data_set["test"]=self.data_loader(procced_data_set,train=False,batch_size=batch_size,shuffle=shuffle)
         
-    def train(self):
-        pass
-    
+    def train(self,lr=0.01,weight_decay=0.0005, epochs=30):
+        self.epoch=epochs
+        param_size = 0
+        for param in self.model.parameters():
+            param_size += param.nelement() * param.element_size()
+        buffer_size = 0
+        for buffer in self.model.buffers():
+            buffer_size += buffer.nelement() * buffer.element_size()
+        size_all_mb = (param_size + buffer_size) / 1024**2
+        size_all_mb = round(size_all_mb,3)
 
+        device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+        self.model = self.model.to(device)
+
+        optimizer = optim.Adam(self.model.parameters(), lr=lr, weight_decay=weight_decay)
+        self.optimizer=optimizer
+        for epoch in range(epochs):
+            self.model.train()
+
+            for i, data in tqdm(enumerate(self.data_set["train"], 0)):
+                optimizer.zero_grad()
+                outputs, m3x3, m64x64 = self.model(data)
+                labels = data['category'].to(device)
+                loss = self.loss_function(outputs, labels, m3x3, m64x64,defualt_dim=10)
+                loss.backward()
+                optimizer.step()
+
+
+            train_acc = self.calculate_accuracy(Train=True)
+            train_loss = self.calculate_loss(Train=True)
+            
+            val_acc = self.calculate_accuracy(Train=False)
+            val_loss = self.calculate_loss(Train=False)
+           
+            print("Epoch: {0} | Train Loss: {1} | Train Acc: {2} | Val Loss: {3} | Val Acc: {4}".format(epoch,train_loss,train_acc,val_loss,val_acc,size_all_mb))
+
+
+    def get_accuracy(self):
+        test_acc = max(self.test_accuracy)
+        train_acc = max(self.train_accuracy)
+        # return round with  3 digits
+        return round(test_acc*100,3),round(train_acc*100,3)
+
+
+        
+    def save_weights(self,path):
+        state_dict = self.model.state_dict()
+        torch.save(state_dict, path)
     
-    def save_weights(self):
-        pass
-    
-    def load_weights(self):
-        pass
+    def load_weights(self,path):
+        self.model.load_state_dict(torch.load(path))
+
+    def save_checkpoint(self,path,loss,acc):
+        torch.save({
+            'epoch': self.epoch,
+            'model_state_dict': self.model.state_dict(),
+            'optimizer_state_dict': self.optimizer.state_dict(),
+            }, path)
+    def load_checkpoint(self,path):
+        checkpoint = torch.load(path)
+        self.model.load_state_dict(checkpoint['model_state_dict'])
+        self.optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+        self.epoch = checkpoint['epoch']
     
     def calculate_accuracy(self,Train=True):
         if Train:
@@ -91,14 +146,18 @@ class MainModel(torch.nn):
         with torch.no_grad():
             self.model.eval()
             correct = 0.
-            model = model.to("cuda")
+            self.model = self.model.to("cuda")
             for data in loader:
-                outputs, m3x3, m64x64 = model(data)
+                outputs, m3x3, m64x64 = self.model(data)
                 labels = data['category'].to("cuda")
                 pred = outputs.max(dim=1)[1]
                 correct += pred.eq(labels).sum().item()
-        return correct / len(loader.dataset)
-    
+        accuracy=correct / len(loader.dataset)
+        if Train:
+            self.train_accuracy.append(accuracy)
+        else:
+            self.test_accuracy.append(accuracy)
+        return accuracy
     def calculate_loss(self,Train=True ):
         if Train:
             loader=self.data_set["train"]
@@ -107,14 +166,17 @@ class MainModel(torch.nn):
         with torch.no_grad():
             self.model.eval()
             loss = 0.
-            model = model.to("cuda")
+            self.model = self.model.to("cuda")
             for data in loader:
-                outputs, m3x3, m64x64 = model(data)
+                outputs, m3x3, m64x64 = self.model(data)
                 labels = data['category'].to("cuda")
                 loss += self.loss_function(outputs, labels).item()
-
-        return loss / len(loader.dataset)
-        
+        loss=loss / len(loader.dataset)
+        if Train:
+            self.train_losses.append(loss)
+        else:
+            self.test_losses.append(loss)
+        return loss
     
     def calculate_confusion_matrix(self):
         pass
