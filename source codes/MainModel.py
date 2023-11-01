281f408aad77c782fb5b6afb50b66cf61feb5d51
Mohamad Choupan <mohamadkhoee@aut.ac.ir>
2023-11-01 16:30:28 +0330

    feat:delete unused libraries

--- source codes/MainModel.py
+++ source codes/MainModel.py
@@ -1,36 +1,10 @@
 # import libraries
 import torch
-from torch import nn
 from torch import optim
 from torch.nn import functional as F
 from torch.utils.data import random_split
 from torch_geometric.data import DataLoader
-
-
-import numpy as np
-import pandas as pd
 import matplotlib.pyplot as plt
-import seaborn as sns
-
-from pathlib import Path
-import os
-import plotly.graph_objects as go
-import math
-import random
-import threading
-from tqdm import tqdm
-import pickle
-
-import torch_geometric
-from torch_geometric.data import Dataset as TGDataset, Data as TGData
-from torch_geometric.loader import DataLoader as TGDataLoader
-from torchvision import transforms, utils
-from torch_geometric.utils.convert import from_networkx
-from torch_geometric import transforms as T
-from torch_geometric.nn import GCNConv,Linear,GATConv,GATv2Conv,SAGEConv, GATConv,ChebConv
-from torch_geometric.nn import GraphConv, TopKPooling
-from torch_geometric.nn import global_mean_pool as gap, global_max_pool as gmp
-from torch_geometric.nn.pool.topk_pool import topk,filter_adj
 
 # Create class for training and testing all models
 class MainModel():
