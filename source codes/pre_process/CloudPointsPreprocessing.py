dddf0c209636b1178215dc058a10d5cd5d126b56
Mohamad Choupan <mohamadkhoee@aut.ac.ir>
2024-03-31 13:48:02 +0330

    retrain

--- source codes/pre_process/CloudPointsPreprocessing.py
+++ source codes/pre_process/CloudPointsPreprocessing.py
@@ -247,8 +247,8 @@ class PointCloudData(Dataset):
                 temp = np.load(graph_feature_path)["arr_0"]
                 empty = []
                 for i in temp:
-                    i = np.append(i[:3])
-                    empty.append(i)
+                #     i = np.append(i[:3])
+                    empty.append(i[:3])
                 print(empty)
                 graph_features = torch.from_numpy(np.array(empty))
                 edge_list = torch.from_numpy(np.load(graph_edge_list_path)["arr_0"])
